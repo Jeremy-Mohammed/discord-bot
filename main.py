f10f8b98c82fc341ecac6f855d31b17c6a4384e5
Jeremy Mohammed <59875684+Jeremy-Mohammed@users.noreply.github.com>
2022-05-19 01:07:10 -0400

    Initial setup
    Music bot beginning and bot integration

---
+++ main.py
@@ -0,0 +1,11 @@
+import discord
+from discord.ext import commands
+import music
+
+cogs
+
+clients = commands.Bot(command_prefix='?', intents = discord.Intents.all())
+
+client.run("OTc2NzA2MTIwNzE2NTUwMTQ0.GgNw4u.8VAVpof7cVbO44c1CSvm-ZlABPNXxAp1tK6xPM")
+
+print("hi")
