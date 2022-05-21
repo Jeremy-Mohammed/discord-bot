9a2c23be8b7e54deddfe0f7cb444272069f12088
Jeremy Mohammed <59875684+Jeremy-Mohammed@users.noreply.github.com>
2022-05-20 23:38:06 -0400

    Update music.py
    Finished play function

--- music.py
+++ music.py
@@ -24,7 +24,19 @@ class music(commands.Cog):
     async def play(self, ctx, url):
         ctx.voice_client.stop()
         FFMPEG_OPTIONS = {'before_options': '-reconnect 1 - reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
-        YDL_OPTIONS = {}
+        YDL_OPTIONS = {'format':'bestaudio'}
+        vc = ctx.voice_client
+
+        with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+            info = ydl.extract_info(url, download = False)
+            url2 = info['format'][0]['url']
+            source = await discord.FFmpeg0pusAudio.from_probe(url2, **FFMPEG_OPTIONS)
+            vc.play(source)
+
+        
+    @commands.command()
+    async def pause(self, ctx):
+        await ctx.voice_client.disconnect()
 
 def setup(client):
     client.add_cog(music(client))
